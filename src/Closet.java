388556751b9b11c542f87e7bfaaf16864135460f
syhan11 <sueyoung.6311@gmail.com>
2019-07-01 09:57:04 -0400

    removed commented out old code

--- src/Closet.java
+++ src/Closet.java
@@ -1,6 +1,6 @@
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
-import java.util.Random;
+
 
 public class Closet {
     private static ArrayList<Jacket> allJackets;
@@ -122,83 +122,5 @@ public class Closet {
 
 
     }
-
-
-
-
-    /***********************
-    public Closet() {
-        this.jacketItem = null;
-        this.shirtItem = null;
-        this.pantsItem = null;
-        this.shoesItem = null;
-    }
-
-    public Closet(Jacket jacketItem, Shirt shirtItem, Pants pantsItem, Shoes shoesItem) {
-        this.jacketItem = jacketItem;
-        this.shirtItem = shirtItem;
-        this.pantsItem = pantsItem;
-        this.shoesItem = shoesItem;
-    }
-
-    public Jacket getJacketItem() {
-        return jacketItem;
-    }
-
-    public void setJacketItem(Jacket jacketItem) {
-        this.jacketItem = jacketItem;
-    }
-
-    public Shirt getShirtItem() {
-        return shirtItem;
-    }
-
-    public void setShirtItem(Shirt shirtItem) {
-        this.shirtItem = shirtItem;
-    }
-
-    public Pants getPantsItem() {
-        return pantsItem;
-    }
-
-    public void setPantsItem(Pants pantsItem) {
-        this.pantsItem = pantsItem;
-    }
-
-    public Shoes getShoesItem() {
-        return shoesItem;
-    }
-
-    public void setShoesItem(Shoes shoesItem) {
-        this.shoesItem = shoesItem;
-    }
-
-    public Jacket lookForJacket(String color){
-        String str = this.jacketItem.getColor();
-
-        if (color.equalsIgnoreCase(str))
-            return  this.jacketItem;
-        else
-            return null;
-    }
-
-    public Shirt lookForShirt(String color){
-        String str = this.shirtItem.getColor();
-
-        if (color.equalsIgnoreCase(str))
-            return  this.shirtItem;
-        else
-            return null;
-    }
-
-    public Pants lookForPants(String color){
-        String str = this.pantsItem.getColor();
-
-        if (color.equalsIgnoreCase(str))
-            return  this.pantsItem;
-        else
-            return null;
-    }
-**************************/
 
 }
--- src/ClosetApp.java
+++ src/ClosetApp.java
@@ -5,13 +5,6 @@ import java.util.Scanner;
 public class ClosetApp {
 
     public static Closet myCloset = new Closet();
-    /*****
-    public static ArrayList<Jacket> allJackets = new ArrayList<Jacket>();
-    public static ArrayList<Shirt> allShirts = new ArrayList<Shirt>();
-    public static ArrayList<Pants> allPants = new ArrayList<Pants>();
-    public static ArrayList<Shoes> allShoes = new ArrayList<Shoes>();
-    *****/
-
     public static void main(String[] args) {
 
         // initialize jacket, shirts, pants, & shoes into ArrayList
@@ -21,11 +14,7 @@ public class ClosetApp {
         myCloset.initalizePants();
         myCloset.initalizeShoes();
 
-
-        //initalizeShirts();
-        //initalizePants();
-        //initalizeShoes();
-
+        // randomly fill suitcases
         Suitcase ansuSuitcase = new Suitcase();
         Suitcase kellySuitcase = new Suitcase();
         Suitcase sueSuitcase = new Suitcase();
@@ -72,75 +61,11 @@ public class ClosetApp {
 
         } while (cont);
 
-
  }
-
-/*****
-    public static void initalizeJackets() {
-        Jacket item = new Jacket("cotton", "blue", true,
-                false, "2 buttons");
-        allJackets.add(item);
-
-        item = new Jacket("linen", "brown", true,
-                false, "3 buttons");
-        allJackets.add(item);
-
-        item = new Jacket("polyester", "grey", true,
-                false, "hood");
-        allJackets.add(item);
-
-        item = new Jacket("linen", "yellow", true,
-                false, "no butons");
-
-
-    }
-****/
-
-/***************
-    public static void initalizeShirts() {
-
-        Shirt item = new Shirt ("linen", "red", true, true, "uTshirt" );
-        allShirts.add(item);
-
-        item = new Shirt ("Denim", "white", true, true, "buttons" );
-        allShirts.add(item);
-
-        item = new Shirt ("Fur", "brown", true, true, "collarshirt" );
-        allShirts.add(item);
-
-        item = new Shirt ("cotton", "brown", true, true, "vTshrit" );
-        allShirts.add(item);
-    }
-
-    public static void initalizePants() {
-        Pants item = new Pants ("linen", "red", true, true, "jean");
-        allPants.add(item);
-
-        item = new Pants ("linen", "red", true, true, "sweat");
-        allPants.add(item);
-
-        item = new Pants ("linen", "red", true, true, "dress");
-        allPants.add(item);
-    }
-
-    public static void initalizeShoes() {
-        // heel, sneakers, sandles, dress, casual (default)
-        Shoes item = new Shoes ("linen", "red", "heel");
-        allShoes.add(item);
-
-        item = new Shoes ("fur", "red", "sneakers");
-        allShoes.add(item);
-
-
-    }
-*********************/
-
-
 
 
     public static void initalizeSuitcase (Suitcase tempSuitcase){
-        //   ArrayList <Jacket> all_of_jackets = new ArrayList <Jacket>();
-        // currently returns one item. add more items later on
+
         Random rand = new Random();
         int id1 = rand.nextInt(myCloset.getJacketNo());
         Jacket oneJacket = myCloset.getJackets(id1);
@@ -158,24 +83,6 @@ public class ClosetApp {
         Shoes oneShoe = myCloset.getShoes(id4);
         tempSuitcase.setShoesItem(oneShoe);
 
-/************
-        int id1 = rand.nextInt(myCloset.allJackets.size());
-        Jacket oneJacket = allJackets.get(id1);
-        tempSuitcase.setJacketItem(oneJacket);
-
-        int id2 = rand.nextInt(allShirts.size());
-        Shirt oneShirt = allShirts.get(id2);
-        tempSuitcase.setShirtItem(oneShirt);
-
-        int id3 = rand.nextInt(allPants.size());
-        Pants onePants = allPants.get(id3);
-        tempSuitcase.setPantsItem(onePants);
-
-        int id4 = rand.nextInt(allShoes.size());
-        Shoes oneShoe = allShoes.get(id4);
-        tempSuitcase.setShoesItem(oneShoe);
-
- *****************/
     }
 
 }
